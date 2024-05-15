e8921d2ab4f7c1e4699a91e0be15fb2ce53c507c
Alexander Winblad <alai20@student.bth.se>
2024-05-15 18:36:02 +0200

    Crud done and working

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 use App\Entity\Book;
 use Doctrine\Persistence\ManagerRegistry;
@@ -28,10 +29,10 @@ class BookController extends AbstractController
         $entityManager = $doctrine->getManager();
 
         $book = new Book();
-        $book->setTitle("The Hunger Games");
-        $book->setIsbn(9780439023481);
-        $book->setAuthor("Suzanne Collins");
-        $book->setImage('hunger_games.jpg');
+        $book->setTitle("Divergent");
+        $book->setIsbn(9789174994711);
+        $book->setAuthor("Veronica Roth");
+        $book->setImage('divergent.webp');
 
         // tell Doctrine you want to (eventually) save the Book
         // (no queries yet)
@@ -90,5 +91,62 @@ class BookController extends AbstractController
         ];
 
         return $this->render('book/viewSingle.html.twig', $data);
+    }
+
+    #[Route('/book/edit/{id}', name: 'book_edit', methods: ['GET', 'POST'])]
+    public function editBook(
+        ManagerRegistry $doctrine,
+        Request $request,
+        int $id
+    ): Response {
+        $entityManager = $doctrine->getManager();
+        $book = $entityManager->getRepository(Book::class)->find($id);
+
+        if (!$book) {
+            throw $this->createNotFoundException(
+                'No book found for id '.$id
+            );
+        }
+
+        if ($request->isMethod('POST')) {
+            $book->setTitle($request->request->get('title'));
+            $book->setAuthor($request->request->get('author'));
+            $book->setIsbn($request->request->get('isbn'));
+
+            $entityManager->flush();
+
+            return $this->redirectToRoute('book_by_id', ['id' => $id]);
+        }
+
+        return $this->render('book/edit.html.twig', [
+            'book' => $book,
+        ]);
+    }
+
+    #[Route('/book/new', name: 'book_new', methods: ['GET', 'POST'])]
+    public function newBook(
+        ManagerRegistry $doctrine,
+        Request $request
+    ): Response {
+        if ($request->isMethod('POST')) {
+            $entityManager = $doctrine->getManager();
+
+            $book = new Book();
+            $book->setTitle($request->request->get('title'));
+            $book->setAuthor($request->request->get('author'));
+            $book->setIsbn($request->request->get('isbn'));
+            $book->setImage("placeholder_book.jpg");
+
+            // tell Doctrine you want to (eventually) save the Book
+            // (no queries yet)
+            $entityManager->persist($book);
+
+            // actually executes the queries (i.e. the INSERT query)
+            $entityManager->flush();
+
+            return $this->redirectToRoute('book_by_id', ['id' => $book->getId()]);
+        }
+
+        return $this->render('book/new.html.twig');
     }
 }
