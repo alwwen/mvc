8ef4d1968d660a20d33e2556b076f9a88f19aa65
Alexander Winblad <alai20@student.bth.se>
2024-05-15 17:28:40 +0200

    Setup the database, create book and delete book

---
+++ src/Controller/BookController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+use App\Entity\Book;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Repository\BookRepository;
+
+class BookController extends AbstractController
+{
+    #[Route('/book', name: 'app_book')]
+    public function index(): Response
+    {
+        return $this->render('book/index.html.twig', [
+            'controller_name' => 'BookController',
+        ]);
+    }
+
+    #[Route('/book/create', name: 'book_create')]
+    public function createBook(
+        ManagerRegistry $doctrine
+    ): Response 
+    {
+        $entityManager = $doctrine->getManager();
+
+        $book = new Book();
+        $book->setTitle("The Hunger Games");
+        $book->setIsbn(9780439023481);
+        $book->setAuthor("Suzanne Collins");
+        $book->setImage('hunger_games.jpg');
+
+        // tell Doctrine you want to (eventually) save the Book
+        // (no queries yet)
+        $entityManager->persist($book);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new book with id '.$book->getId());
+    }
+
+    #[Route('/library', name: 'book_show_all')]
+    public function showAllBook(
+        BookRepository $bookRepository
+    ): Response {
+        $books = $bookRepository
+            ->findAll();
+
+        $data = [
+            'books' => $books
+        ];
+
+        return $this->render('book/view.html.twig', $data);
+    }
+
+    #[Route('/book/delete/{id}', name: 'book_delete_by_id', methods: ['POST'])]
+    public function deleteBookById(
+        ManagerRegistry $doctrine,
+        int $id
+    ): Response {
+        $entityManager = $doctrine->getManager();
+        $book = $entityManager->getRepository(Book::class)->find($id);
+
+        if (!$book) {
+            throw $this->createNotFoundException(
+                'No book found for id '.$id
+            );
+        }
+
+        $entityManager->remove($book);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('book_show_all');
+    }
+
+    #[Route('/book/show/{id}', name: 'book_by_id')]
+    public function showProductById(
+        BookRepository $bookRepository,
+        int $id
+    ): Response {
+        $book = $bookRepository
+            ->find($id);
+
+        $data = [
+            'book' => $book
+        ];
+
+        return $this->render('book/viewSingle.html.twig', $data);
+    }
+}
